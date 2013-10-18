4caee9e7f5ea46e3af70d0ff99c2c4eac1bf53bb
Sergio Diaz <alu0100696615@ull.edu.es>
2013-10-18 12:04:09 +0100

    Multiplicacion hecha

--- racional.rb
+++ racional.rb
@@ -4,13 +4,24 @@
 require "./gcd.rb"
 
 class Fraccion
+attr_reader :x, :y
  def initialize(x,y)#x=numerador, y=denominador
   @x,@y=x, y
  end
  def to_s
   "#{@x}/#{@y}"
  end
+ def *(obj)
+  Fraccion.new(@x*obj.x, @y*obj.y) 
+ end
+
 end
 
 a= Fraccion.new(1,2)
 puts "#{a}"
+
+b= Fraccion.new(3,3)
+puts "#{b}"
+
+c= a* b
+puts "#{c}"
