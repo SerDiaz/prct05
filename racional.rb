533932a9993839af1f0ec8a28ee72938de929a96
Sergio Diaz <alu0100696615@ull.edu.es>
2013-10-18 11:54:59 +0100

    Añadido to_s y el initialize

--- racional.rb
+++ racional.rb
@@ -1,6 +1,16 @@
+#! usr/bin/ruby
 # Implementar en este fichero la clase para crear objetos racionales
 
 require "./gcd.rb"
 
 class Fraccion
+ def initialize(x,y)#x=numerador, y=denominador
+  @x,@y=x, y
+ end
+ def to_s
+  "#{@x}/#{@y}"
+ end
 end
+
+a= Fraccion.new(1,2)
+puts "#{a}"
