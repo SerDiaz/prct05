90038d86237e54e3a964114295341058e3f9249f
Sergio Diaz <alu0100696615@ull.edu.es>
2013-10-18 12:06:07 +0100

    division hecha

--- racional.rb
+++ racional.rb
@@ -15,13 +15,19 @@ attr_reader :x, :y
   Fraccion.new(@x*obj.x, @y*obj.y) 
  end
 
+ def /(obj)
+  Fraccion.new(@x*obj.y, @y*obj.x) 
+ end
 end
 
 a= Fraccion.new(1,2)
 puts "#{a}"
 
-b= Fraccion.new(3,3)
+b= Fraccion.new(5,3)
 puts "#{b}"
 
 c= a* b
+puts "#{c}"
+
+c= a/ b
 puts "#{c}"
