e1f70f5a83db6f0ace6b0ab08a3741b521a9ec42
Sergio Diaz <alu0100696615@ull.edu.es>
2013-10-18 12:54:11 +0100

    Hecho varias pruebas unitarias

--- tc_racional.rb
+++ tc_racional.rb
@@ -4,5 +4,10 @@ require "./racional.rb"
 require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
-
+ def test_simple
+    assert_equal("2/3", Fraccion.new(2,3).to_s )
+    assert_equal("3/4", Fraccion.new(3,4).to_s )
+    assert_not_equal("7,6",Fraccion.new(1,2).to_s) 
+    assert_not_equal("4,3",Fraccion.new(5,2).to_s) 
+ end
 end
